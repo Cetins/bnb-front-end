4d8b600bb020872679ef829397ff837ecc3d4bd4
Cetins <69481095+Cetins@users.noreply.github.com>
2022-08-25 16:34:28 +0100

    property single and list component added

--- src/properties/PropertyItem.jsx
+++ src/properties/PropertyItem.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-
 const PropertyItem = ({ property }) => {
 
     const propertyHeading = `${property.bedCount} bed ${property.type} in ${property.location}`
-
     return  (
-        <React.Fragment>
+        <div className="test" >
             <h2>{propertyHeading}</h2>
+            <h3>£ {property.rate} per night</h3> 
+            <img className="list-cover-img" src={require('../images/' + property.coverImage + '.jpg')} />
             <p>{property.description}</p>
-        </React.Fragment>
+        </div>
     )
 
 }
