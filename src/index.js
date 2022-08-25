282f4223d47318ee9a47ef6523d1629b9c13ae50
Cetins <69481095+Cetins@users.noreply.github.com>
2022-08-25 16:32:35 +0100

    button, imh styling added, connected to index js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
+import "./styles/button.css";
+import "./styles/images.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
