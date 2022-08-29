0c68bfcaec6e2158e70f6815e94cf0ff4376addc
Cetins <69481095+Cetins@users.noreply.github.com>
2022-08-29 12:38:42 +0100

    property page connected with components

--- src/properties/Property.jsx
+++ src/properties/Property.jsx
@@ -1,21 +1,40 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import Rating from "./preview-card/Rating";
 import PropertyService from "../services/PropertyService";
+import AmenityService from "../services/AmenityService";
 import Description from "./Description";
 import CheckInAfter from "./CheckInAfter";
 import CheckOutBefore from "./CheckOutBefore";
 import ReviewList from "../reviews/ReviewList";
+import IconContainer from "../properties/preview-card/IconContainer";
+import AmenityList from "./amenities/AmenityList";
+import FacilityList from "./facilities/FacilityList";
+import ParkingOptions from "./parking/ParkingOptions";
+import PropertyRulesList from "./property_rules/PropertyRulesList";
 
 const Property = () => {
 
     const {id} = useParams();
     const [property, setProperty] = useState(null);
+    const [amenities, setAmenities] = useState([]);
+    const [amenityCategories, setAmenityCategories] = useState([]);
 
     useEffect(() => {
         PropertyService.findProperty(id)
         .then(property => setProperty(property));
-    });
+    }, []);
+
+    useEffect(() => {
+        AmenityService.getAmenities()
+        .then(amenities => setAmenities(amenities));
+    }, []);
+
+    useEffect(() => {
+        const categories = amenities.map(amenity => amenity.category)
+
+        const uniqueCategories = [...new Set(categories)];
+        setAmenityCategories(uniqueCategories);
+    }, [amenities]);
     
     if(!property) {
         return <p>Loading...</p>
@@ -23,33 +42,43 @@ const Property = () => {
     
     return  (
         <div>
-            <span className="rating-span">
-                <Rating rating={property.rating} />
-                <h3> , { property.location }</h3>
-            </span>
+            <div>
+                <IconContainer property={property} />
+            </div>
             <br/>
 
-            <img className="cover-img" src={require('../static/images/property/' + property.coverImage + '.jpg')} />
-            <br/>
+            <div>
+                <img className="cover-img" src={require('../static/images/property/' + property.coverImage + '.jpg')} />
+                <br/>
+            </div>
 
-            <div className="card">
-                <div>
-                    <h2>{property.type} in {property.location}, hosted by {property.host.firstName}</h2>
-                </div>
+            <div>
+                <h2>{property.type} in {property.location}, hosted by {property.host.firstName}</h2>
+            </div>
 
-                <div>
-                    <Description text={property.description} />
-                </div>
-                    
-                <div>
-                    <CheckInAfter time={property.checkInAfter} />
-                    <CheckOutBefore time={property.checkOutBefore} />
-                </div>
-
-                <div>
-                    <ReviewList  reviews={property.reviews}/>
-                </div>
-            </div> 
+            <div>
+                <Description text={property.description} />
+            </div>
+            <div>
+                <AmenityList categories={amenityCategories} amenities={property.amenities} />
+            </div>
+            <div>
+                <FacilityList facilities={property.facilities} />
+            </div>
+            <div>
+                <CheckInAfter time={property.checkInAfter} /><br/>
+                <CheckOutBefore time={property.checkOutBefore} />
+            </div>
+            {property.parkingOptions.length != 0 ? 
+            <div>
+                <ParkingOptions parkingOptions={property.parkingOptions}/>
+            </div> : null}
+            <div>
+                <PropertyRulesList propertyRules={property.propertyRules} />
+            </div>
+            <div>
+                <ReviewList  reviews={property.reviews}/>
+            </div>
         </div>
     )
 }
