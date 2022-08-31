0e1e5e17d233f82ca3901becd91193a0755da4d5
Cetins <69481095+Cetins@users.noreply.github.com>
2022-08-31 15:21:21 +0100

    gallery fixed

--- src/gallery/Gallery.jsx
+++ src/gallery/Gallery.jsx
@@ -1,26 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Gallery = ({ imageUrls }) => {
+const Gallery = ({ property, imageUrls }) => {
     const url_1 = imageUrls[0].url;
     const url_2 = imageUrls[1].url;
     const url_3 = imageUrls[2].url;
     const url_4 = imageUrls[3].url;
     const url_5 = imageUrls[4].url;
 
+    const url = "/property/gallery/" + property.id;
+
     return (
-        <div className="gallery-div-row">
-            <img className="gallery-img-1" src={require('../static/images/property/' + url_1 + '.jpg')} />
-            <div className="gallery-div-row">
-                <div className="gallery-div-column">
-                    <img className="gallery-img-2" src={require('../static/images/property/' + url_2 + '.jpg')} />
-                    <img className="gallery-img-3" src={require('../static/images/property/' + url_3 + '.jpg')} />
-                </div>
-                <div className="gallery-div-column">
-                    <img className="gallery-img-4" src={require('../static/images/property/' + url_4 + '.jpg')} />
-                    <img className="gallery-img-5" src={require('../static/images/property/' + url_5 + '.jpg')} />
+        <Link to={url} >
+            <div className="row-wrap">
+                <img className="gallery-img-1" src={require('../static/images/property/' + url_1 + '.jpg')} />
+                <div className="row-wrap">
+                    <div className="column-wrap">
+                        <img className="gallery-img-2" src={require('../static/images/property/' + url_2 + '.jpg')} />
+                        <img className="gallery-img-3" src={require('../static/images/property/' + url_3 + '.jpg')} />
+                    </div>
+                    <div className="column-wrap">
+                        <img className="gallery-img-4" src={require('../static/images/property/' + url_4 + '.jpg')} />
+                        <img className="gallery-img-5" src={require('../static/images/property/' + url_5 + '.jpg')} />
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
